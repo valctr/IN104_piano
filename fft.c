cfc84837c02a7996b1add24a5fbf2c61d8c8f760
Catrou Valentin <cristobal.catrou@gmail.com>
2022-04-05 15:36:45 +0200

    first commit

---
+++ fft.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "wav.h"
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fft_complex.h>
+#define REAL(z,i) ((z)[2*(i)])
+#define IMAG(z,i) ((z)[2*(i)+1])
+
+
+int main(){
+char fichieraudio[100];
+unsigned int taille;
+printf ("entrer le nom du fichier audio a analyser :\n");
+scanf("%s", fichieraudio);
+printf ("nom du fichier : %s\n", fichieraudio);
+
+
+double *data = open_wav(fichieraudio,"data.dat");
+
+gsl_complex_packed_array data_fft = data;
+gsl_fft_complex_radix2_forward(data_fft, 1, 128);
+
+FILE *fft=fopen("data_fft.dat","w");
+for (int i = 0; i < 128; i++)
+{
+printf ("%d %e %e\n", i,
+REAL(data_fft,i)/sqrt(128),
+IMAG(data_fft,i)/sqrt(128));
+}
+return 0;
+}
+

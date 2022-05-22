81dde94d0a7bec2ef843f908d0583c824f3604ce
Catrou Valentin <cristobal.catrou@gmail.com>
2022-05-22 18:01:47 +0200

    solving problems with the fft size

--- spectrogram/fft.c
+++ spectrogram/fft.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 #include "wav.h"
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_fft_complex.h>
 #define REAL(z,i) ((z)[2*(i)])
 #define IMAG(z,i) ((z)[2*(i)+1])
-
+#define FFT_SIZE 8192
 
 
 int main(){
@@ -15,20 +15,24 @@ printf ("entrer le nom du fichier audio a analyser :\n");
 scanf("%s", fichieraudio);
 printf ("nom du fichier : %s\n", fichieraudio);
 
-int taille=0;
-double *data = open_wav(fichieraudio,"data.dat",&taille);
-printf("tailleres : %d\n",taille);
+int length;
+double *data = open_wav(fichieraudio,"data.dat", &length);
 
+/*double tmp[length]; 
+for (int i=0;i<length;i++) {
+	REAL(tmp,i) = REAL(data,i);
+	IMAG(tmp,i) = IMAG(data,j);
+}*/
 gsl_complex_packed_array data_fft = data;
-gsl_fft_complex_radix2_forward(data_fft, 1, taille);
+gsl_fft_complex_radix2_forward(data_fft, 1, FFT_SIZE); //calcul de la fft
 
-FILE *fft=fopen("data_fft.dat","w");
-for (int i = 0; i < taille; i++)
+FILE *fft=fopen("data_fft.dat","w"); //réécriture des résultats dans un fichier .dat
+for (int i = 0; i < FFT_SIZE; i++)
 {
 fprintf (fft,"%lf %lf\n", REAL(data_fft,i), IMAG(data_fft,i));
 }
+
 free(data);
-free(data_fft);
 return 0;
 }
 
---
+++ spectrogram/wav.h
@@ -0,0 +1,22 @@
+#ifndef _MON_MODULE_H
+#define _MON_MODULE_H
+
+struct wavfile //définit la structure de l entete d un wave
+{
+char id[4]; // doit contenir "RIFF"
+int totallength; // taille totale du fichier moins 8 octets
+char wavefmt[8]; // doit etre "WAVEfmt "
+int format; // 16 pour le format PCM
+short pcm; // 1 for PCM format
+short channels; // nombre de channels
+int frequency; // frequence d echantillonage
+int bytes_per_second; // nombre de octets par secondes
+short bytes_by_capture; // nombre de bytes par echantillon
+short bits_per_sample; // nombre de bit par echantillon
+char data[4]; // doit contenir "data"
+int bytes_in_data; // nombre de bytes de la partie data
+};
+
+
+double* open_wav();
+#endif
